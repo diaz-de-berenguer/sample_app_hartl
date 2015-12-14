677b31e39fba55c0891c8ed1c34ad15fc56c534f
Diego Diaz de Berenguer <d.diazdeberenguer@gmail.com>
2015-12-14 00:01:54 -0600

    chapter 4 in tutorial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	
+	# Returns the full title on a per-page basis.
+	def full_title (page_title = '')
+		if page_title.empty?
+			"Sample App"
+		else
+			page_title + " | Sample App"
+		end
+	end
+
 end
