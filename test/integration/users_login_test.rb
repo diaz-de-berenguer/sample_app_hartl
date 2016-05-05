561f115f4c8a3d74f719ad902e8a80959dfbeb89
Diego Diaz de Berenguer <d.diazdeberenguer@gmail.com>
2016-05-05 15:03:46 -0500

    Finish 8.3

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,15 +16,23 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		assert flash.empty?
 	end
 
-	test "log in with valid information" do
+	test "log in with valid information followed by log out" do
 		get login_path
 		assert_template 'sessions/new'
 		post login_path, session: { email: 'diego@example.com', password: 'password' }
+		assert is_logged_in?
 		assert_redirected_to @user
 		follow_redirect!
 		assert_template 'users/show'
 		assert_select "a[href=?]", login_path, count: 0
 		assert_select "a[href=?]", logout_path
 		assert_select "a[href=?]", user_path(@user)
+		delete logout_path
+		assert_not is_logged_in?
+		assert_redirected_to root_url
+		follow_redirect!
+		assert_select "a[href=?]", login_path
+		assert_select "a[href=?]", logout_path, count: 0
+		assert_select "a[href=?]", user_path(@user), count: 0
 	end
 end
